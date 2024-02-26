b0f0740c697f18be307ad6f04820453303118b83
Lalita Pawar <83364539+lalita-pawar@users.noreply.github.com>
2024-02-26 22:33:33 +0530

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,11 @@
+public class Food {
+    private int x;
+    private int y;
+   // Implement methods to generate new food at random positions on the game board
+
+    public Food(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    // These methods can be added here or in the GamePanel class depending on design
+}
